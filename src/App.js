f520bcaf5ef9f00a75992b28dddf8a91b0314471
deenu-cse <153585196+deenu-cse@users.noreply.github.com>
2024-09-14 12:44:44 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './name'
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setQuery(value);
+
+
+    if (value) {
+      const filteredSuggestions = data.filter(item =>
+        item.country.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  // Function to handle click on a suggestion
+  const handleSuggestionClick = (suggestion) => {
+    setQuery(suggestion.country);
+    setSuggestions([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="search-container">
+      <img
+        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+        alt="Google Logo"
+        className="logo"
+      />
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search"
+          value={query}
+          onChange={handleChange}
+        />
+        <button className="search-button">
+          <img
+            src="https://img.icons8.com/?size=100&id=7695&format=png&color=000000"
+            alt="Search Icon"
+          />
+        </button>
+      </div>
+      {suggestions.length > 0 && (
+        <div className="suggestions">
+          {suggestions.map((suggestion, index) => (
+            <div
+              key={index}
+              className="suggestion-item"
+              onClick={() => handleSuggestionClick(suggestion)}
+            >
+              {suggestion.country}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
